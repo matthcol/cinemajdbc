5fd6a707b6f6cebfb0f70714716664841a166429
Matthias Colin <matthias.colin@gmail.com>
2019-09-06 10:44:30 +0200

    add insert exemples

--- src/test/java/cinema/test/TestCinema.java
+++ src/test/java/cinema/test/TestCinema.java
@@ -3,6 +3,7 @@ package cinema.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -44,6 +45,43 @@ class TestCinema {
 			}
 		} 	// fin utilisation  resources
 			// auto : res.close(), st.close(), conn.close()
+	}
+	
+	@Test
+	void testEcriture() throws SQLException {
+		String sql = "insert into film (titre, annee, duree, num_real) "
+				+ "values ('Once Upon a Time... in Hollywood', 2019, 161, 14)";
+		try ( // resources
+				Connection conn = ds.getConnection();
+				Statement st = conn.createStatement();
+		) {
+			int nbRowInserted = st.executeUpdate(sql);
+			System.out.println("Film ajouté : " + nbRowInserted);
+		}
+	}
+	
+	@Test
+	void testEcritureFromHuman() throws SQLException {
+		// data from form
+		String titre = "Andy";
+		int annee = 2019;
+		int duree = 91;
+		int num_real = 12;
+		
+		// request with 4 placeholders
+		String sql = "insert into film (titre, annee, duree, num_real) "
+				+ "values (?,?,?,?)";
+		try ( // resources
+				Connection conn = ds.getConnection();
+				PreparedStatement st = conn.prepareStatement(sql);
+		) {
+			st.setString(1, titre);
+			st.setInt(2, annee);
+			st.setInt(3, duree);
+			st.setInt(4, num_real);
+			int nbRowInserted = st.executeUpdate();
+			System.out.println("Film ajouté : " + nbRowInserted);
+		}
 	}
 	
 }
